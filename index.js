a0b09469a8fd4d5cbf7bc654348bf687bbf8a60f
veritedonato <veritedonato@gmail.com>
2023-04-21 11:12:27 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,95 +1,94 @@
 
 
-function object(e,x,y,w,h,c,t,tc) {
+class object {
+constructor(e,x,y,w,h,c,t,tc) {
 
-  this.context = function(canvas) {
+this.context=function(canvas) {
 
-    return document.getElementById(canvas).getContext('2d');
-  
-  }
+return document.getElementById(canvas).getContext('2d');
 
-  this.ctx = this.context(e);
-  this.x = x;
-  this.y = y;
-  this.c = c;
-  this.t = t;
-  this.w = w;
-  this.h = h;
-  this.tc = tc;
-  
-  this.render = function () {
+};
 
-      this.ctx.fillStyle =  this.c;
-      this.ctx.fillRect(this.x,this.y,this.w,this.h);
-      this.ctx.fillStyle = this.tc;
-      this.ctx.fillText(this.t,this.x+10,this.y+10);      
+this.ctx=this.context(e);
+this.x=x;
+this.y=y;
+this.c=c;
+this.t=t;
+this.w=w;
+this.h=h;
+this.tc=tc;
 
-  }
+this.render=function() {
 
-  this.move = function (x,y) {
+this.ctx.fillStyle=this.c;
+this.ctx.fillRect(this.x,this.y,this.w,this.h);
+this.ctx.fillStyle=this.tc;
+this.ctx.fillText(this.t,this.x+10,this.y+10);
 
-      this.x += x;
-      this.y += y;
+};
 
-  }  
-  
-  this.click = function () {
+this.move=function(x,y) {
+
+this.x+=x;
+this.y+=y;
+
+};
+
+this.click=function() {
+};
+
+this.keydown=function(e) {
+
+if(e.keyCode==37) {
+
+this.move(-64,0);
+
+}
+if(e.keyCode==39) {
+
+this.move(64,0);
 
 
+}
+if(e.keyCode==38) {
 
-  }
+this.move(0,-64);
 
-  this.keydown = function (e) {
 
-    if ( e.keyCode == 37 ) {
+}
+if(e.keyCode==40) {
 
-          this.move(-64,0);
+this.move(0,64);
 
-    }
-    if ( e.keyCode == 39 ) {
-
-      this.move(64,0);
-
-        
-    }
-    if ( e.keyCode == 38 ) {
-
-      this.move(0,-64);
-
-        
-    }
-    if ( e.keyCode == 40 ) {
-
-      this.move(0,64);
-
-        
-    }
-
-  }
-  
-  this.addClickEvent() {
-
-    window.addEventListener("onclick",this.click);
 
 }
 
-this.addKeyEvent() {
+};
 
-    window.addEventListener("onkeydown",this.keydown);
+this.addClickEvent=function() {
+
+window.addEventListener("onclick",this.click);
+
+};
+
+this.addKeyEvent=function() {
+
+window.addEventListener("onkeydown",this.keydown);
+
+};
 
 }
-
 }
 
 var monsters = [];
-var numMonsters = 5;
+var numMonsters = 0;
 var monsterChance = 5;
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function main() {
+function create() {
 
   var background = new object('canvas',0,0,window.innerWidth,window.innerHeight,'black','Welcome to Aezaria.','yellow');
   var player = new object('canvas',256,256,64,64,'green','Player','yellow');
@@ -98,9 +97,6 @@ function main() {
   player.render();
 
 
-  for ( var m = 0; m < numMonsters; m++ )
-
-    {
 
       for ( var x = 0; x < window.innerWidth; x += 64 ) {
         for ( var y = 0; y < window.innerHeight; y += 64 ) {
@@ -115,6 +111,8 @@ function main() {
 
       monsters.push(monster);
 
+                numMonsters++;
+
               }
 
         }
@@ -122,37 +120,53 @@ function main() {
       }
 
 
-    }
 
 
-    for ( var m = 0; m < numMonsters; m++ ) {
+    player.addKeyEvent();
 
-        var monster = monsters[m];
+}
 
-        var chance =  getRandomInt(1000);
-
-        if ( chance < 250 )
-
-          monster.move(0,-64);
-
-        if ( chance > 250 && chance < 500 )
-
-            monster.move(0,64);
+function update() {
 
 
-        if ( chance > 500 && chance < 750 )
+  for ( var m = 0; m < numMonsters; m++ ) {
 
-            monster.move(64,0);
-
-
-        if ( chance > 750 && chance < 1000 )
-
-            monster.move(-64,0);
+    var monster = monsters[m];
 
 
-    }
+      console.log("monster");
+
+
+    var chance =  getRandomInt(1000);
+
+    if ( chance < 250 )
+
+      monster.move(0,-64);
+
+    if ( chance > 250 && chance < 500 )
+
+        monster.move(0,64);
+
+
+    if ( chance > 500 && chance < 750 )
+
+        monster.move(64,0);
+
+
+    if ( chance > 750 && chance < 1000 )
+
+        monster.move(-64,0);
+
+      monster.render();
+
+}
+
 
 
 }
 
-setInterval(main(),100);
+
+
+setInterval(update(),100);
+
+create();
